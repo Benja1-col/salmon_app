eb887fba4da319e5ba6f53c23908c1cfb814f070
Benja1-col <benjasolis0291@gmail.com>
2025-05-02 02:29:01 -0400

    Update test_conexion.py

--- test_conexion.py
+++ test_conexion.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 
 try:
-    # Intenta conectarte al servidor MongoDB local
     client = MongoClient("mongodb://localhost:27017/")
     
-    # Intenta obtener el nombre de las bases de datos
     dbs = client.list_database_names()
     
     print("Conexión exitosa a MongoDB.")
